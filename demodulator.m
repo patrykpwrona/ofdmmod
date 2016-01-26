9071a4480895d9c1e5941ce7da488454e098f64c
patrykwro <patryk.wrona92@gmail.com>
2016-01-26 02:04:44 +0100

    modulator + trochę demodulatora

---
+++ demodulator.m
@@ -0,0 +1,41 @@
+%wchodz¹ nam dane rzeczywiste szeregowo
+8888
+
+N=4;
+fs=16;
+
+%liczymy fft porcjami po step punktów
+
+step=fs*N;
+
+for i=1:length(data_in)/N
+    data_demodulated((i-1)*step+1:(i-1)*step+step)=fft(data_serialized((i-1)*step+1:(i-1)*step+step));
+end
+
+% serial to parallel
+for i=1:fs*length(data_in)/N
+    for j=1:N
+        data_demodulated_parallel(j,i)=data_demodulated((i-1)*N+j);       
+    end
+end
+
+%demapping
+%bierzemy kolejne 16 symboli z ka¿dego oczepu sumujemy je i jeszcze raz
+%liczymy jeddno punktow¹ fft
+%nie ma to ¿adnego uzasadnienia naukowego
+for i=i:length(data_in)/N
+    for j=1:N
+        data_demapped(j,i)=data_demodulated_parallel(
+    end
+end
+
+%demodulacja, ka¿de 16 próbek powinno teraz zostaæ potraktowane jako symbol
+%i zdekodowane jako 1 lub 0
+
+
+
+
+%figure(5)
+%hold on;
+%plot(real(data_demodulated),'r');
+%plot(imag(data_demodulated),'b');
